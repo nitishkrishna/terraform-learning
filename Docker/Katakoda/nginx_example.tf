bd8186ae5bfd7ea5df600101fbcb78647fa67e4a
Nitish Krishna <nitishk@juniper.net>
2019-01-14 16:31:03 -0800

    Docker Nginx Example: https://www.katacoda.com/courses/terraform

---
+++ Docker/Katakoda/nginx_example.tf
@@ -0,0 +1,24 @@
+# This TF file deploys an nginx docker container on specified docker host
+
+# Connect to docker host
+provider "docker" {
+  host = "tcp://docker:2345/"
+}
+
+# Pick the docker image
+resource "docker_image" "nginx" {
+  name = "nginx:1.11-alpine"
+}
+
+resource "docker_container" "nginx-server" {
+  name = "nginx-server"
+  image = "${docker_image.nginx.latest}"
+  ports {
+    internal = 80
+  }
+  volumes {
+    container_path  = "/usr/share/nginx/html"
+    host_path = "/home/scrapbook/tutorial/www"
+    read_only = true
+  }
+}
