31a8e69ff2953af2034f5038a97ff4a6a6a8903f
Nitish Krishna <nitishk@juniper.net>
2019-01-15 13:49:50 -0800

    Formatted changes

--- GCP/gcp_example.tf
+++ GCP/gcp_example.tf
@@ -1,9 +1,11 @@
 // Configure the Google Cloud provider
 provider "google" {
-  #
+  # These are read as ENV vars (source.env before terraform plan)
+  # GOOGLE_APPLICATION_CREDENTIALS
+  # GCLOUD_PROJECT
   # credentials = "${file("gcp_terraform_project_creds.json")}"
   # project     = "terraform-learning"
-  region      = "us-west1"
+  region = "us-west1"
 }
 
 // Terraform plugin for creating random ids
@@ -23,32 +25,34 @@ resource "google_compute_instance" "flask-vm" {
     initialize_params {
       image = "debian-cloud/debian-9"
     }
- }
+  }
 
- // Make sure flask is installed on all new instances for later steps
- metadata_startup_script = "sudo apt-get update; sudo apt-get install -yq build-essential python-pip rsync; pip install flask"
+  // Make sure flask is installed on all new instances for later steps
+  metadata_startup_script = "sudo apt-get update; sudo apt-get install -yq build-essential python-pip rsync; pip install flask"
 
- metadata {
-   sshKeys = "nitishk:${file(var.google_cloud_ssh_pub_key_path)}"
- }
+  metadata {
+    sshKeys = "nitishk:${file(var.google_cloud_ssh_pub_key_path)}"
+  }
 
- network_interface {
-   network = "default"
-   access_config {
-     // Include this section to give the VM an Ephemeral external ip address
-   }
- }
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Include this section to give the VM an Ephemeral external ip address
+    }
+  }
 }
 
 # Add firewall to allow HTTP access
 resource "google_compute_firewall" "default" {
- name    = "flask-app-firewall"
- network = "default"
+  name    = "flask-app-firewall"
+  network = "default"
 
- allow {
-   protocol = "tcp"
-   ports    = ["5000"]
- }
+  allow {
+    protocol = "tcp"
+    ports    = ["5000"]
+  }
 }
 
-# Access Flask app at: <External IP>:5000
+# Access Flask app at: http://<External IP>:5000
+
--- GCP/variables.tf
+++ GCP/variables.tf
@@ -1,5 +1,5 @@
 variable "google_cloud_ssh_pub_key_path" {
   description = "Filepath for SSH Pub Key"
-  type = "string"
-  default = "google-cloud-key.pub"
+  type        = "string"
+  default     = "google-cloud-key.pub"
 }
